ea2a98e3adff2f6820b6adb8f09896cbde8f26fa
Trung Kiên <92300304+kientthe163256@users.noreply.github.com>
2023-12-20 17:40:03 +0700

    feat: dockerize project

--- api-gateway/src/main/java/com/example/springmicroservice/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/springmicroservice/ApiGatewayApplication.java
@@ -1,16 +1,26 @@
 package com.example.springmicroservice;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
+    @Value("${spring.application.name}")
+    private String name;
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("--------------APP_NAME===========" + name);
     }
 }
--- product-service/src/main/java/com/example/springmicroservice/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/springmicroservice/productservice/ProductServiceApplication.java
@@ -1,15 +1,35 @@
 package com.example.springmicroservice.productservice;
 
+import com.example.springmicroservice.productservice.model.Product;
+import com.example.springmicroservice.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import java.math.BigDecimal;
+
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProductServiceApplication {
+@RequiredArgsConstructor
+public class ProductServiceApplication implements CommandLineRunner {
+
+    private final ProductRepository productRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        if (productRepository.count() < 1) {
+            Product product = new Product();
+            product.setName("iPhone 13");
+            product.setDescription("iPhone 13");
+            product.setPrice(BigDecimal.valueOf(1000));
+
+            productRepository.save(product);
+        }
+    }
 }
